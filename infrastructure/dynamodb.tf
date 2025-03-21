5629768b2234aca69fbdec3fe3e004d915dc31ed
Thinh Le <thinh.le@bellevuecollege.edu>
2025-03-20 23:47:11 -0700

    Modified folders and files

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -1,7 +1,18 @@
 resource "aws_dynamodb_table" "california_violations" {
-  name           = "CaliforniaViolations"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "plate"
+  name         = "CaliforniaViolations"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "plate"
+
+  attribute {
+    name = "plate"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "out_of_state_violations" {
+  name         = "OutOfStateViolations"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "plate"
 
   attribute {
     name = "plate"
--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -2,4 +2,14 @@ resource "aws_sqs_queue" "upward_queue" {
   name                      = "UpwardQueue"
   message_retention_seconds = 345600
   visibility_timeout_seconds = 30
+  receive_wait_time_seconds  = 10
+  sqs_managed_sse_enabled    = true
+}
+
+resource "aws_sqs_queue" "downward_queue" {
+  name                      = "DownwardQueue"
+  message_retention_seconds = 345600
+  visibility_timeout_seconds = 30
+  receive_wait_time_seconds  = 0
+  sqs_managed_sse_enabled    = true
 }
